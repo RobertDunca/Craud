d46bd95c6b1e46dda787ea395ec1d7d117884315
obert Dunca <duradi.mail@gmail.com>
2022-09-28 16:53:08 +0300

    added reactive search bar in nav bar

--- static/js/my_js.js
+++ static/js/my_js.js
@@ -1,3 +1,5 @@
+// limit text length in listview
+
 var max = 150;
 var tot, str;
 $('.text').each(function () {
@@ -8,4 +10,16 @@ $('.text').each(function () {
         : str.substring(0, (max + 1)) + "...";
     $(this).html(str);
 });
+
+// reveal search bar after scrolling 200px
+
+$(window).scroll(function () {
+    if ($(this).scrollTop() > 200) {
+        $('.fixed-element').css({'display': 'block'});
+    } else {
+        $('.fixed-element').css({'display': 'none'});
+    }
+});
+
+
 
