581e9eeea5a12644ab2f3733beb0244e19a776b7
obert Dunca <duradi.mail@gmail.com>
2022-10-30 16:59:47 +0200

    minor changes

--- trip/models.py
+++ trip/models.py
@@ -1,7 +1,6 @@
 from django.contrib.auth.models import User
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
-from modelcluster.fields import ParentalManyToManyField
 
 from userextend.models import User
 
